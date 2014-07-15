55fbed287d39e9020fb17fe360db06da47c80bd9
Jay Hariani <jay@govtribe.com>
2014-07-15 16:44:00 -0400

    WIP

--- esproxy.php
+++ esproxy.php
@@ -1,13 +1,11 @@
 <?php
-
     $ch = curl_init();
-    $method = "GET";
-    $url = "search.govtribe.com/project/_search?pretty=true";
+    $method = "POST";
+    $url = "search.govtribe.com/project/_search";
     $username = '';
     $password = '';
      
     $query = file_get_contents('php://input');
-    $query = json_decode($query);
      
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_PORT, 9200);
